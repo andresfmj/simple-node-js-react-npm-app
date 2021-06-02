839effb5d7661db50e82d365f144e92c7b517da7
Andres Fernando <andres.fernando08@gmail.com>
2021-06-02 16:23:28 -0500

    updated App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to DentOS</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is placeholder text just for continous integration purposes.
         </p>
+        <p>Hello</p>
       </div>
     );
   }
